d526ff6549f7e45b8a1c49aeb61deb270e04a14b
William Foster <21318458@bucks.ac.uk>
2018-10-04 13:07:24 +0100

    Modified site master

---
+++ MusicWebsite/Models/Music.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicWebsite.Models
+{
+    public class Music
+    {
+        public int MusicID { get; set; }
+
+        public string Band { get; set; }
+        
+    }
+}
